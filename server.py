900227a72d163fb185d4c3f922a8b520b2342994
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-04-15 22:41:37 -0500

    Bug Fix

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
 import db
+import config
 
 connection = db.Connection(config.db["host"], config.db["db"], config.db["user"], config.db["password"])
 app = Flask(__name__)

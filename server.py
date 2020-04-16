9d058f327ef804784f70dcd9fd98af1eb5e80da2
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-04-15 23:53:42 -0500

    Update to use bottle instead of flask

--- server.py
+++ server.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
 from json import dumps
+import bottle
 import db
 import config
 
 connection = db.Connection(config.db["host"], config.db["db"], config.db["user"], config.db["password"])
-app = Flask(__name__)
-api = Api(app)
 
-
-class general_exposure(Resource):
-    def get(self):
-        results = []
-        genExps = connection.read("SELECT ingredient_id, ingredient_name, general_exposure_count, general_exposure FROM public.general_exposures limit 50;")
-        for ge in genExps:
-            results.append(ge)
-        return {
-            "results": [r for r in results]
-            }
+@bottle.get('/general_exposure')
+def index():
+    results = []
+    genExps = connection.read("SELECT ingredient_id, ingredient_name, general_exposure_count, general_exposure FROM public.general_exposures limit 50;")
+    for ge in genExps:
+        results.append(ge)
+    return {
+        "results": [r for r in results]
+        }
     
 
-api.add_resource(general_exposure, '/general_exposure') # Route_1
 
+# ---- Errors
+@bottle.error(404)
+def error404(error):
+  bottle.response.set_header("Content-Type", "application/json")
+  return "{\"error\": \"unrecognized URL\"}"
 
-if __name__ == '__main__':
-     app.run()
+# - SERVER -
+if config.use_prod_webserver:
+  bottle.run(host='0.0.0.0', port=80, server="gunicorn")
+else:
+  bottle.run(host='0.0.0.0', port=80, debug=True, reloader=True)
